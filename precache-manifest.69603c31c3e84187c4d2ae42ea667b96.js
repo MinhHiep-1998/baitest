d487b1243b9f37b5201baaab8c31c533150c1421
hackintosh-linxhq <hiepdtm@linxhq.com>
2020-09-30 10:47:18 +0700

    Updates

---
+++ precache-manifest.69603c31c3e84187c4d2ae42ea667b96.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2be05864070e20e69b6dda1075af75e8",
+    "url": "/baitest/index.html"
+  },
+  {
+    "revision": "6b5af7a1e1d1a5e58f18",
+    "url": "/baitest/static/css/main.ce477ac6.chunk.css"
+  },
+  {
+    "revision": "6ee66eb338975ae98345",
+    "url": "/baitest/static/js/2.d77251c9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/baitest/static/js/2.d77251c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b5af7a1e1d1a5e58f18",
+    "url": "/baitest/static/js/main.d0ecb6f0.chunk.js"
+  },
+  {
+    "revision": "5281026cb2c335095954",
+    "url": "/baitest/static/js/runtime-main.bd84f3c2.js"
+  }
+]);
